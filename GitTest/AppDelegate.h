eb659a32b917283727f117505f1b13e917e2d82f
wangshuang <wangshuang@wangshuangdemac-mini.local>
2018-04-26 15:14:49 +0800

    Initial Commit

---
+++ GitTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitTest
+//
+//  Created by wangshuang on 2018/4/26.
+//  Copyright © 2018年 wangshuang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
